d470e5e92834b6a8a18b1443d97747d00ba29745
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-11 15:58:32 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,3 +1,4 @@
+import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/pages/app_routes.dart';
 import 'package:demo0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
@@ -8,8 +9,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final loginBloc = BlocProvider(create: (context) => LoginBloc());
+
     return MultiBlocProvider(
-      providers: [],
+      providers: [loginBloc],
       child: MaterialApp(
         title: "CMApp",
         routes: AppRoute.all,
