1c1a7991b36f25733bbda1da7c2c2b9626f8a0b4
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-11 10:25:00 +0700

    1234

--- demo0/lib/main.dart
+++ demo0/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 main() {
   runApp(App());
@@ -9,6 +9,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Text("Home"),
+    );
   }
 }
