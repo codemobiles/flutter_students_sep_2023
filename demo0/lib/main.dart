149c0c9fdd6557552d64264d55359832d53f92e3
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-11 10:27:39 +0700

    1234

--- demo0/lib/main.dart
+++ demo0/lib/main.dart
@@ -10,7 +10,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Home"),
+      home: LoginPage(),
     );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
