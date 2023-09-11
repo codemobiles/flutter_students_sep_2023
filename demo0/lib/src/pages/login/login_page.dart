9862b3f088d143337b0d03b5ec936a2caeaf2923
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-11 11:52:57 +0700

    1234

---
+++ demo0/lib/src/pages/login/login_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login")),
+      body: Container(
+        width: double.infinity,
+        color: Colors.red.shade100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MyBox(title: "Box1"),
+            MyBox(title: "Box2", color: Colors.yellow),
+            MyBox(title: "Box3", color: Colors.orange),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyBox extends StatelessWidget {
+  const MyBox({required this.title, super.key, this.color});
+
+  final String title;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color ?? Colors.pink,
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 30, color: Colors.white),
+      ),
+    );
+  }
+}
