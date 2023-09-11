719820d0a95bc89c6054f0ddaa96fae20e450401
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-11 15:56:57 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,16 +1,20 @@
 import 'package:demo0/src/pages/app_routes.dart';
 import 'package:demo0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "CMApp",
-      routes: AppRoute.all,
-      home: LoginPage(),
+    return MultiBlocProvider(
+      providers: [],
+      child: MaterialApp(
+        title: "CMApp",
+        routes: AppRoute.all,
+        home: LoginPage(),
+      ),
     );
   }
 }
