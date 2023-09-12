ad9964786ce48a971e3ad1d77dd046514406f48a
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 15:55:37 +0700

    1234

--- demo0/lib/src/bloc/home/home_state.dart
+++ demo0/lib/src/bloc/home/home_state.dart
@@ -2,7 +2,7 @@ part of 'home_bloc.dart';
 
 enum FetchStatus { fetching, success, failed, init }
 
-sealed class HomeState extends Equatable {
+class HomeState extends Equatable {
   const HomeState({
     this.products = const [],
     this.status = FetchStatus.init,
@@ -12,6 +12,17 @@ sealed class HomeState extends Equatable {
   final List<Product> products;
   final FetchStatus status;
   final bool isGrid;
+
+  HomeState copyWith({
+    List<Product>? products,
+    FetchStatus? status,
+    bool? isGrid,
+  }) {
+    return HomeState(
+        products: products ?? this.products,
+        status: status ?? this.status,
+        isGrid: isGrid ?? this.isGrid);
+  }
 
   @override
   List<Object> get props => [];
