f17b00b23a5c210430d19d417114420d7850f4b5
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 13:25:11 +0700

    1234

--- demo0/lib/src/pages/home/home_page.dart
+++ demo0/lib/src/pages/home/home_page.dart
@@ -12,13 +12,126 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Home')),
-        drawer: const Drawer(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Menu"),
-          ],
-        )),
+        drawer: CustomDrawer(),
         body: Text("Home"));
   }
+}
+
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  void _showDialogBarcode(context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) => const DialogBarcodeImage(
+        'www.codemobiles.com',
+      ),
+    );
+  }
+
+  void _showDialogQRImage(context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) => const DialogQRImage(
+        'www.codemobiles.com',
+      ),
+    );
+  }
+
+  void _showScanQRCode(context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext dialogContext) => const DialogScanQRCode(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          _buildProfile(),
+          ListTile(
+            onTap: () => _showDialogBarcode(context),
+            title: const Text("BarCode"),
+            leading:
+                const Icon(Icons.bar_chart_outlined, color: Colors.deepOrange),
+          ),
+          ListTile(
+            onTap: () => _showDialogQRImage(context),
+            title: const Text("QRCode"),
+            leading: const Icon(Icons.qr_code, color: Colors.green),
+          ),
+          ListTile(
+            onTap: () => _showScanQRCode(context),
+            title: const Text("Scanner"),
+            leading: const Icon(Icons.qr_code_scanner, color: Colors.blueGrey),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.map),
+            title: const Text("Map"),
+            leading: const Icon(Icons.map_outlined, color: Colors.blue),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.sqlite),
+            title: const Text("SQLite3 (Option)"),
+            leading: const Icon(Icons.table_rows_sharp, color: Colors.blue),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_push),
+            title: const Text("FirebasePush (Option)"),
+            leading: const Icon(Icons.message, color: Colors.yellow),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_auth),
+            title: const Text("FirebaseAuth (Option)"),
+            leading: const Icon(Icons.local_fire_department_rounded,
+                color: Colors.orange),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_store),
+            title: const Text("FirebaseStore (Option)"),
+            leading: const Icon(Icons.storage, color: Colors.red),
+          ),
+          ListTile(
+            onTap: () =>
+                Navigator.pushNamed(context, AppRoute.firebase_analytics),
+            title: const Text("FirebaseAnalytic (Option)"),
+            leading: const Icon(Icons.analytics, color: Colors.purpleAccent),
+          ),
+          const Spacer(),
+          _buildLogoutButton(),
+        ],
+      ),
+    );
+  }
+
+  UserAccountsDrawerHeader _buildProfile() => UserAccountsDrawerHeader(
+        currentAccountPicture: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+          ),
+          child: const CircleAvatar(
+            backgroundImage: AssetImage(Asset.cmLogoImage),
+          ),
+        ),
+        accountName: const Text('CMDev'),
+        accountEmail: const Text('support@codemobiles.com'),
+      );
+
+  Builder _buildLogoutButton() => Builder(
+        builder: (context) => SafeArea(
+          child: ListTile(
+              leading: const Icon(Icons.exit_to_app),
+              title: const Text('Log out'),
+              onTap: () => context.read<LoginBloc>().add(LoginEventLogout())),
+        ),
+      );
 }
