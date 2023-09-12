e2aa0758450367407f28484ab041e02f13dede88
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 16:07:34 +0700

    1234

--- demo0/lib/src/bloc/home/home_bloc.dart
+++ demo0/lib/src/bloc/home/home_bloc.dart
@@ -12,9 +12,13 @@ part 'home_state.dart';
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
     on<HomeEventLoadProducts>((event, emit) async {
+      emit(state.copyWith(products: [], status: FetchStatus.fetching));
+      await Future.delayed(const Duration(seconds: 1));
+
       final dio = Dio();
       final result = await dio.get(NetworkAPI.baseURL + "/products");
       final products = productFromJson(jsonEncode(result.data));
+      emit(state.copyWith(products: products, status: FetchStatus.success));
     });
   }
 }
