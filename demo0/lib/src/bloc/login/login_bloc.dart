36524098a48a896b22bd3ea2e2eefe8386c5eb44
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 10:05:19 +0700

    1234

--- demo0/lib/src/bloc/login/login_bloc.dart
+++ demo0/lib/src/bloc/login/login_bloc.dart
@@ -6,8 +6,14 @@ part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState(count: 99)) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
+    // Add Event
+    on<LoginEventAdd>((event, emit) {
+      emit(state.copyWith(count: state.count + 1));
+    });
+
+    // Remove Event
+    on<LoginEventRemove>((event, emit) {
+      emit(state.copyWith(count: state.count - 1));
     });
   }
 }
