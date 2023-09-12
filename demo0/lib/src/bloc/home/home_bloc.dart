48327b2119ae9ae72762c4022bd532cc7d213774
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 16:05:52 +0700

    1234

--- demo0/lib/src/bloc/home/home_bloc.dart
+++ demo0/lib/src/bloc/home/home_bloc.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/models/product.dart';
+import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 
 part 'home_event.dart';
@@ -7,8 +11,10 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
-    on<HomeEvent>((event, emit) {
-      // TODO: implement event handler
+    on<HomeEventLoadProducts>((event, emit) async {
+      final dio = Dio();
+      final result = await dio.get(NetworkAPI.baseURL + "/products");
+      final products = productFromJson(jsonEncode(result.data));
     });
   }
 }
