80fc761d7ca8c639519beb8ea8593ab97d415517
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-12 14:32:04 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,10 +1,13 @@
 import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/pages/app_routes.dart';
+import 'package:demo0/src/pages/home/home_page.dart';
 import 'package:demo0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'constants/network_api.dart';
 
 final formatCurrency = NumberFormat('#,###.000');
 final formatNumber = NumberFormat('#,###');
@@ -28,8 +31,25 @@ class App extends StatelessWidget {
     );
   }
 
-  _initialPage() async {
-    final prefs = await SharedPreferences.getInstance();
-    return LoginPage();
+  _initialPage() {
+    // final prefs = await SharedPreferences.getInstance();
+    // return LoginPage();
+
+    return FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text("Loading...");
+        } else {
+          final prefs = snapshot.data!;
+          final token = prefs.getString(NetworkAPI.token);
+          if (token != null) {
+            return HomePage();
+          } else {
+            return LoginPage();
+          }
+        }
+      },
+    );
   }
 }
