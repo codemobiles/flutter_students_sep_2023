251c9ca304ed7795d939638341f2d97dee7d2b1d
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-13 11:05:34 +0700

    1234

--- demo0/lib/src/pages/home/home_page.dart
+++ demo0/lib/src/pages/home/home_page.dart
@@ -1,34 +1,25 @@
-import 'dart:convert';
-
-import 'package:another_flushbar/flushbar.dart';
 import 'package:demo0/src/bloc/home/home_bloc.dart';
 import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/constants/asset.dart';
-import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/models/product.dart';
 import 'package:demo0/src/pages/app_routes.dart';
 import 'package:demo0/src/pages/home/widgets/dialog_barcode_image.dart';
 import 'package:demo0/src/pages/home/widgets/dialog_qr_image.dart';
 import 'package:demo0/src/pages/home/widgets/dialog_scan_qrcode.dart';
-import 'package:demo0/src/pages/home/widgets/my_product_item.dart';
 import 'package:demo0/src/pages/home/widgets/product_item.dart';
-import 'package:demo0/src/widgets/custom_flushbar.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  _HomePageState createState() => _HomePageState();
 }
-
-// final vs const
 
 class _HomePageState extends State<HomePage> {
   @override
-  initState() {
+  void initState() {
     super.initState();
 
     context.read<HomeBloc>().add(HomeEventLoadProducts());
@@ -36,69 +27,125 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // final homeState = context.watch<HomeBloc>().state;
+    // final homeState = BlocProvider.of<HomeBloc>(context, listen: true).state;
+
     return Scaffold(
+        backgroundColor: Colors.grey[200],
+        drawer: const CustomDrawer(),
         appBar: AppBar(
-          title: const Text('Home'),
+          title: const Text("Home BlocV2"),
           actions: [
-            IconButton(onPressed: () {
-              context.read<HomeBloc>().add(HomeEventToggleDisplay());
-            }, icon: BlocBuilder<HomeBloc, HomeState>(
-              builder: (context, state) {
-                return Icon(state.isGrid ? Icons.grid_3x3 : Icons.list);
-              },
-            )),
+            IconButton(
+              onPressed: () =>
+                  context.read<HomeBloc>().add(HomeEventToggleDisplay()),
+              icon: BlocBuilder<HomeBloc, HomeState>(
+                builder: (context, state) {
+                  return state.isGrid
+                      ? const Icon(Icons.grid_3x3)
+                      : const Icon(Icons.list);
+                },
+              ),
+            )
           ],
         ),
-        drawer: CustomDrawer(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _navigateManagementPage(),
+          child: const Icon(Icons.add),
+        ),
         body: BlocBuilder<HomeBloc, HomeState>(
           builder: (context, state) {
             final products = state.products;
-
-            return state.isGrid
-                ? _buildGrid(products: products)
-                : _buildList(products: products);
+            return RefreshIndicator(
+              onRefresh: () async {
+                context.read<HomeBloc>().add(HomeEventLoadProducts());
+              },
+              child: Container(
+                child: state.isGrid
+                    ? _buildGridView(products)
+                    : _buildListView(products),
+              ),
+            );
           },
         ));
   }
 
-  _buildGrid({required List<Product> products}) {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        crossAxisSpacing: 1,
-        mainAxisSpacing: 1,
-        childAspectRatio: 0.9, // set height ratio -  (itemWidth / itemHeight)
-      ),
-      itemCount: products.length,
+  _navigateManagementPage([Product? product]) async {
+    await Navigator.pushNamed(context, AppRoute.management, arguments: product);
+    context.read<HomeBloc>().add(HomeEventLoadProducts());
+  }
+
+  Widget _buildListView(List<Product> products) {
+    return ListView.builder(
       itemBuilder: (context, index) {
-        return SizedBox(
-            height: 320,
-            child: MyProductItem(
-              product: products[index],
-              onPressed: () {
-                CustomFlushbar.showSuccess(context,
-                    message: "${products[index].name}");
-              },
-            ));
+        if (index == 0) {
+          return Column(
+            children: [
+              _buildHeader(),
+              Padding(
+                padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+                child: SizedBox(
+                  height: 350,
+                  child: ProductItem(
+                    product: products[index],
+                    onTap: () => _navigateManagementPage(products[index]),
+                  ),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return Padding(
+            padding: const EdgeInsets.only(left: 8, right: 8),
+            child: SizedBox(
+              height: 350,
+              child: ProductItem(
+                product: products[index],
+                onTap: () => _navigateManagementPage(products[index]),
+              ),
+            ),
+          );
+        }
       },
+      itemCount: products.length,
     );
   }
 
-  _buildList({required List<Product> products}) {
-    return ListView.builder(
-      itemCount: products.length,
-      itemBuilder: (context, index) {
-        return SizedBox(
-            height: 320,
-            child: MyProductItem(
-              product: products[index],
-              onPressed: () {
-                CustomFlushbar.showSuccess(context,
-                    message: "${products[index].name}");
-              },
-            ));
-      },
+  Widget _buildGridView(List<Product> products) {
+    return Column(
+      children: [
+        _buildHeader(),
+        Expanded(
+          child: GridView.builder(
+            padding: const EdgeInsets.only(top: 10),
+            itemBuilder: (context, index) {
+              return ProductItem(
+                isGrid: true,
+                product: products[index],
+                onTap: () => _navigateManagementPage(products[index]),
+              );
+            },
+            itemCount: products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 1,
+              childAspectRatio:
+                  0.9, // set height ratio -  (itemWidth / itemHeight)
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  _buildHeader() {
+    return Container(
+        child: Image.asset(
+      Asset.logoImage,
+      height: 100,
+      width: double.infinity,
+    ));
   }
 }
 
@@ -138,103 +185,61 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            _buildProfile(),
-            ListTile(
-              onTap: () => _showDialogBarcode(context),
-              title: const Text("BarCode"),
-              leading: const Icon(Icons.bar_chart_outlined,
-                  color: Colors.deepOrange),
-            ),
-            ListTile(
-              onTap: () => _showDialogQRImage(context),
-              title: const Text("QRCode"),
-              leading: const Icon(Icons.qr_code, color: Colors.green),
-            ),
-            ListTile(
-              onTap: () => _showScanQRCode(context),
-              title: const Text("Scanner"),
-              leading:
-                  const Icon(Icons.qr_code_scanner, color: Colors.blueGrey),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushNamed(context, AppRoute.map),
-              title: const Text("Map"),
-              leading: const Icon(Icons.map_outlined, color: Colors.blue),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushNamed(context, AppRoute.sqlite),
-              title: const Text("SQLite3 (Option)"),
-              leading: const Icon(Icons.table_rows_sharp, color: Colors.blue),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushNamed(context, AppRoute.firebase_push),
-              title: const Text("FirebasePush (Option)"),
-              leading: const Icon(Icons.message, color: Colors.yellow),
-            ),
-            ListTile(
-              onTap: () => Navigator.pushNamed(context, AppRoute.firebase_auth),
-              title: const Text("FirebaseAuth (Option)"),
-              leading: const Icon(Icons.local_fire_department_rounded,
-                  color: Colors.orange),
-            ),
-            ListTile(
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoute.firebase_store),
-              title: const Text("FirebaseStore (Option)"),
-              leading: const Icon(Icons.storage, color: Colors.red),
-            ),
-            ListTile(
-              leading: Icon(Icons.abc, color: Colors.pink),
-              trailing: Icon(Icons.inbox),
-              title: Text("FirebaseAnalytic (Option)"),
-              onTap: () => _showDialog(context),
-            ),
-            ListTile(
-              leading: Icon(Icons.abc, color: Colors.pink),
-              trailing: Icon(Icons.inbox),
-              title: Text("FirebaseAnalytic (Option)"),
-              onTap: () => _showDialog(context),
-            ),
-            ListTile(
-              leading: Icon(Icons.abc, color: Colors.pink),
-              trailing: Icon(Icons.inbox),
-              title: Text("FirebaseAnalytic (Option)"),
-              onTap: () => _showDialog(context),
-            ),
-            _buildLogoutButton(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showDialog(BuildContext context) {
-    // print("Hey");
-    showDialog(
-      context: context,
-      builder: (context) {
-        return Dialog(
-          child: SizedBox(
-            height: 250,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Text("I am lek"),
-                  ElevatedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("Close"))
-                ],
-              ),
-            ),
+      child: Column(
+        children: [
+          _buildProfile(),
+          ListTile(
+            onTap: () => _showDialogBarcode(context),
+            title: const Text("BarCode"),
+            leading:
+                const Icon(Icons.bar_chart_outlined, color: Colors.deepOrange),
           ),
-        );
-      },
+          ListTile(
+            onTap: () => _showDialogQRImage(context),
+            title: const Text("QRCode"),
+            leading: const Icon(Icons.qr_code, color: Colors.green),
+          ),
+          ListTile(
+            onTap: () => _showScanQRCode(context),
+            title: const Text("Scanner"),
+            leading: const Icon(Icons.qr_code_scanner, color: Colors.blueGrey),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.map),
+            title: const Text("Map"),
+            leading: const Icon(Icons.map_outlined, color: Colors.blue),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.sqlite),
+            title: const Text("SQLite3 (Option)"),
+            leading: const Icon(Icons.table_rows_sharp, color: Colors.blue),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_push),
+            title: const Text("FirebasePush (Option)"),
+            leading: const Icon(Icons.message, color: Colors.yellow),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_auth),
+            title: const Text("FirebaseAuth (Option)"),
+            leading: const Icon(Icons.local_fire_department_rounded,
+                color: Colors.orange),
+          ),
+          ListTile(
+            onTap: () => Navigator.pushNamed(context, AppRoute.firebase_store),
+            title: const Text("FirebaseStore (Option)"),
+            leading: const Icon(Icons.storage, color: Colors.red),
+          ),
+          ListTile(
+            onTap: () =>
+                Navigator.pushNamed(context, AppRoute.firebase_analytics),
+            title: const Text("FirebaseAnalytic (Option)"),
+            leading: const Icon(Icons.analytics, color: Colors.purpleAccent),
+          ),
+          const Spacer(),
+          _buildLogoutButton(),
+        ],
+      ),
     );
   }
 
