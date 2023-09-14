5e0b705fd31ab6c459fd14a6e9123ea0ccf28f90
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-14 15:15:51 +0700

    1234

--- demo0/lib/src/bloc/map/map_state.dart
+++ demo0/lib/src/bloc/map/map_state.dart
@@ -1,12 +1,14 @@
 part of 'map_bloc.dart';
 
 class MapState extends Equatable {
-  const MapState();
-  
+  const MapState({required this.currentPosition});
+
   final LatLng currentPosition;
-  
+
+  MapState copyWith({LatLng? currentPosition}) {
+    return MapState(currentPosition: currentPosition ?? this.currentPosition);
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [currentPosition];
 }
-
-
