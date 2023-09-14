57e6863c2dbc4da83476c2a7815c2514581bdd19
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-14 15:21:35 +0700

    1234

--- demo0/lib/src/bloc/map/map_bloc.dart
+++ demo0/lib/src/bloc/map/map_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/services/network_service.dart';
 import 'package:equatable/equatable.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -7,6 +8,9 @@ part 'map_state.dart';
 
 class MapBloc extends Bloc<MapEvent, MapState> {
   MapBloc() : super(MapState(currentPosition: const LatLng(0, 0))) {
-    on<MapEventSubmitLocation>((event, emit) {});
+    on<MapEventSubmitLocation>((event, emit) {
+      emit(state.copyWith(currentPosition: event.position!));
+      NetworkService().submitLocation(event.position!);
+    });
   }
 }
