5a3c8946e83154694576be08e5d3cd088949f3d7
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-14 09:37:26 +0700

    1234

---
+++ demo0/lib/src/pages/home/widgets/my_dialog_barcode_image.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyDialogBarCode extends StatelessWidget {
+  const MyDialogBarCode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Text("1234"),
+    );
+  }
+}
