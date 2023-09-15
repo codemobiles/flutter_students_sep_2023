9f36cd299be29fbc8df3483b1280391d66decfd8
chaiyasit tayabovorn <chaiyasit.t@gmail.com>
2023-09-15 14:13:11 +0700

    1234

--- demo0/lib/src/pages/webview/web_page.dart
+++ demo0/lib/src/pages/webview/web_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class WebPage extends StatefulWidget {
   const WebPage({super.key});
@@ -8,13 +9,30 @@ class WebPage extends StatefulWidget {
 }
 
 class _WebPageState extends State<WebPage> {
+  late InAppWebViewController _webViewController;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Web'),
       ),
-      body: Container(),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: InAppWebView(
+          initialUrlRequest: URLRequest(url: Uri.parse("https://pospos.co")),
+          initialOptions: InAppWebViewGroupOptions(
+            crossPlatform: InAppWebViewOptions(),
+          ),
+          onLoadStart: (controller, url) {
+            print("Page started loading: $url");
+          },
+          onLoadStop: (controller, url) {
+            print("Page finished loading: $url");
+          },
+        ),
+      ),
     );
   }
 }
